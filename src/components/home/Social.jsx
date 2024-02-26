66d928d94e8fadd071aa988d53a257369ba5ecb0
Arsh <upadhyayarsh23@gmail.com>
2024-02-26 11:48:58 +0530

    About Section

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://github.com/arshupadhyay" className="home__social-icon" target='_blank'>
+            <i class="uil uil-github"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/arsh-upadhyay-701225247/" className="home__social-icon" target='_blank'>
+            <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a href="#" className="home__social-icon" target='_blank'>
+            <i class="uil uil-instagram"></i>
+        </a>
+    </div>
+  );
+}
+
+export default Social;
